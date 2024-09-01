5063d0cedd89f0a12af3c1e4d210027fc6a58cb5
Gil <gilvicentevieira@gmail.com>
2024-09-01 23:01:14 +0100

    trigger

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -19,7 +19,7 @@ const generateClassName = createGenerateClassName({
 
 const history = createBrowserHistory();
 
-// container app 
+// container application
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
